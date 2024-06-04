fe96ed2bd19cf1304dfb01f9995a55042aa919af
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-04 11:25:28 +0300

    connected the login page to signup page

--- app/src/main/java/com/akirachix/mentorconnect/LoginActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/LoginActivity.kt
@@ -5,19 +5,17 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val connectRegister = findViewById<TextView>(R.id.tv_register)
-        connectRegister.setOnClickListener{
-            val intent = Intent(this,LoginActivity::class.java)
+        setContentView(R.layout.activity_login)
+        
+        val connectLogin = findViewById<TextView>(R.id.tv_login)
+        connectLogin.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
-
+        
     }
-    }
+}
