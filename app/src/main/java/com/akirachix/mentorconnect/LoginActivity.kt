d06f930a052b11dbfe645219505791c97f40ab03
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-05 20:06:45 +0300

    first commit

--- app/src/main/java/com/akirachix/mentorconnect/LoginActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/LoginActivity.kt
@@ -3,19 +3,64 @@ package com.akirachix.mentorconnect
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.akirachix.mentorconnect.databinding.ActivityLoginBinding
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        
-        val connectLogin = findViewById<TextView>(R.id.tv_login)
-        connectLogin.setOnClickListener{
-            val intent = Intent(this,MainActivity::class.java)
-            startActivity(intent)
-        }
-        
+        binding = ActivityLoginBinding.inflate(layoutInflater) //the inflate layout it is used to initiate the binding view
+
+        setContentView(binding.root)
+
+       binding.tvLogin.setOnClickListener{
+           finish()
+//            startActivity(Intent(this,MainActivity::class.java))  //you can just finish instead of creating another intent, bc login from login
+
+       // Another way to connect other than binding method
+//        setContentView(R.layout.activity_login)
+//
+//        val connectLogin = findViewById<TextView>(R.id.tvLogin)
+//        connectLogin.setOnClickListener{
+//            val intent = Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+
+
+    }
+    binding.btnLogin.setOnClickListener {
+        validateLogin()
+    }
+
+}
+
+fun validateLogin() {
+
+    clearErrors()
+    var formError = false
+
+    val userName2 = binding.etUsername2.text.toString()
+    if (userName2.isBlank()) {
+        formError = true
+        binding.tilUsername2.error = "username is required"
+    }
+
+    val password2 = binding.etPassword2.text.toString()
+    if (password2.isBlank()) {
+        formError = true
+        binding.tilPassword2.error = "password is required"
+    }
+    if (!formError) {
+        //
     }
 }
+fun clearErrors() {
+    binding.tilUsername2.error = null
+    binding.tilPassword2.error = null
+
+
+}
+}
+
--- app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
+++ app/src/main/java/com/akirachix/mentorconnect/MainActivity.kt
@@ -3,21 +3,99 @@ package com.akirachix.mentorconnect
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.akirachix.mentorconnect.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val connectRegister = findViewById<TextView>(R.id.tv_register)
-        connectRegister.setOnClickListener{
-            val intent = Intent(this,LoginActivity::class.java)
+        binding.tvRegister.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+
+//        val connectRegister = findViewById<TextView>(R.id.tvRegister)
+//        connectRegister.setOnClickListener{
+//            val intent = Intent(this,LoginActivity::class.java)
+//            startActivity(intent)
+        }
+        binding.btnRegister.setOnClickListener {
+            validateRegistration()
         }
 
     }
+
+    fun validateRegistration() {
+        clearErrors()
+        var formError = false
+
+        val firstName = binding.etFirstName.text.toString()
+        if (firstName.isBlank()) {
+            formError = true
+            binding.tilFirstName.error = "First name is required"
+        }
+
+        val lastName = binding.etLastName.text.toString()
+        if (lastName.isBlank()) {
+            formError = true
+            binding.tilLastName.error = "Last name is required"
+        }
+
+        val email = binding.etEmail.text.toString()
+        if (email.isBlank()) {
+            formError = true
+            binding.tilEmail.error = "email address is required"
+        }
+
+        val codeHiveId = binding.etId.text.toString()
+        if (codeHiveId.isBlank()) {
+            formError = true
+            binding.tilId.error = "CodeHive id  is required"
+        }
+
+        val userName = binding.etUsername.text.toString()
+        if (userName.isBlank()) {
+            formError = true
+            binding.tilUsername.error = "email address is required"
+        }
+
+        val password = binding.etPassword.text.toString()
+        if (password.isBlank()) {
+            formError = true
+            binding.tilPassword.error =
+                getString(R.string.password_is_required)  // we had this "Password is required" then we extracted it so as to avoid hard coding
+        }
+
+        val passwordConf = binding.etConfirmPassword.text.toString()
+        if (passwordConf.isBlank()) {
+            formError = true
+            binding.tilConfirmPassword.error = getString(R.string.password_confirmation_is_required)
+        }
+        if (password != passwordConf) {
+            formError = true
+            binding.tilConfirmPassword.error =
+                getString(R.string.password_and_confirmation_do_not_match)  // we had a normal text then, click on it and extract from the yellow bulb
+            // bcs we are not supposed to hard code the string
+        }
+        if (!formError) {
+            //
+        }
     }
+
+        fun clearErrors() {
+            binding.tilFirstName.error = null
+            binding.tilLastName.error = null
+            binding.tilEmail.error = null
+            binding.tilId.error = null
+            binding.tilUsername.error = null
+            binding.tilPassword.error = null
+            binding.tilConfirmPassword.error = null
+
+
+        }
+    }
+
